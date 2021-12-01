b0fca0b0871698aa8367b074776d23c31510d082
sigmanificient <sigmanificient@gmail.com>
2021-12-01 23:59:29 +0100

    :sparkles: Day 1 part 2

--- 2021/1.py
+++ 2021/1.py
@@ -6,5 +6,11 @@ def day_1_part_1(data):
     return sum(x < y for x, y in zip(data, data[1:]))
 
 
+@provide
+def day_1_part_2(data):
+    return sum(a < d for a, d in zip(data, data[3:]))
+
+
 if __name__ == '__main__':
     day_1_part_1()
+    day_1_part_2()
