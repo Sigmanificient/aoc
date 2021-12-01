f1f53754d466274a3f5a89662dc5b13013aaeff7
sigmanificient <sigmanificient@gmail.com>
2021-12-02 00:03:33 +0100

    :recycle: Extracting logic

--- 2021/1.py
+++ 2021/1.py
@@ -1,14 +1,21 @@
 from wrapper import provide
 
 
+def _count_larger_sum(first_seq, offset):
+    return sum(
+        x < y
+        for x, y in zip(first_seq, offset)
+    )
+
+
 @provide
 def day_1_part_1(data):
-    return sum(x < y for x, y in zip(data, data[1:]))
+    return _count_larger_sum(data, 1)
 
 
 @provide
 def day_1_part_2(data):
-    return sum(a < d for a, d in zip(data, data[3:]))
+    return _count_larger_sum(data, 3)
 
 
 if __name__ == '__main__':
