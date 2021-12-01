12ce858148230217012cd9f3d882f1600b574d90
sigmanificient <sigmanificient@gmail.com>
2021-12-02 00:11:35 +0100

    :memo: improving doc

--- wrapper.py
+++ wrapper.py
@@ -1,5 +1,22 @@
-def provide(func):
-    def wrapped():
+from typing import List, Callable
+
+
+def provide(func: Callable[[List[int]], None]) -> ():
+    """
+    Decorator to provide the data to the function
+        and prints the result.
+
+    Parameters
+    ----------
+    func : function
+        The function to be decorated.
+
+    Returns
+    -------
+    func : function
+        The decorated function.
+    """
+    def wrapped() -> None:
         with open("../input.data") as f:
             row_data = f.read()
 
