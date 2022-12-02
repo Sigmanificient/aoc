950843a1fce3ef2b5c642056d2fc0ddd4af8ff14
sigmanificient <edhyjox@gmail.com>
2022-12-02 11:31:30 +0100

    :sparkles: Add solution for part 2

--- 2022/day_1/main.py
+++ 2022/day_1/main.py
@@ -1,20 +1,25 @@
 import pathlib
-from collections import defaultdict
+from typing import List
+
+
+def get_group_scores(data: List[str]) -> List[int]:
+    elves = [0]
+
+    for line in data:
+        if line:
+            elves[-1] += int(line)
+        else:
+            elves.append(0)
+
+    return sorted(elves)
 
 
 def main():
     content = pathlib.Path('./input.txt').read_text()
+    elves = get_group_scores(content.splitlines())
 
-    elves = defaultdict(int)
-    idx = 0
-
-    for line in content.split('\n'):
-        if not line:
-            idx += 1
-        else:
-            elves[idx] += int(line)
-
-    print('Part 1:', max(elves.values()))
+    print('Part 1:', elves[-1])
+    print('Part 2:', sum(elves[-3:]))
 
 
 if __name__ == '__main__':
