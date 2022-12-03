83fd9b13769a89cbab5088d78649a594f5498020
sigmanificient <edhyjox@gmail.com>
2022-12-03 17:07:59 +0100

    :sparkles: Add solution for day 3 - part 2

--- 2022/day_3/main.py
+++ 2022/day_3/main.py
@@ -1,21 +1,33 @@
 import pathlib
 
 
-def get_letter_index(char: str) -> int:
-    i = ord(char) # magic done here :3
+def get_priority(items):
+    item = set.intersection(*map(set, items)).pop()
+    i = ord(item)  # magic done here :3
     return (i | 32) - 70 - (26 * (i & 32) >> 5)
+
+
+def get_compartments_priorities_sum(rucksacks):
+    def cut_line(line):
+        half = len(line) // 2
+        return map(set, (line[:half], line[half:]))
+
+    return sum(map(get_priority, map(cut_line, rucksacks)))
+
+
+def get_badge_priorities_sum(rucksacks):
+    def chunk(lst, size):
+        return [lst[i:i + size] for i in range(0, len(lst), size)]
+
+    return sum(map(get_priority, chunk(rucksacks, 3)))
 
 
 def main():
     content = pathlib.Path('./input.txt').read_text()
-    total = 0
+    lines = content.splitlines()
 
-    for line in content.splitlines():
-        half = len(line) // 2
-        left, right = map(set, (line[:half], line[half:]))
-        total += get_letter_index(left.intersection(right).pop())
-
-    print("Part 1:", total)
+    print("Part 1:", get_compartments_priorities_sum(lines))
+    print("Part 2:", get_badge_priorities_sum(lines))
 
 
 if __name__ == '__main__':
