e71399bec03cbdb89098ec905d6e046476a46086
sigmanificient <edhyjox@gmail.com>
2022-12-06 22:36:41 +0100

    :sparkles: Add solution for day 6 part 2

--- 2022/day_6/main.py
+++ 2022/day_6/main.py
@@ -1,7 +1,7 @@
 import pathlib
 
 
-def get_first_non_repeating(sequence, buffer_size) -> int:
+def get_first_non_repeating(sequence: str, buffer_size: int) -> int:
     buf = [''] * buffer_size
     idx = 0
     while len(set(x for x in buf if x)) != buffer_size:
@@ -14,6 +14,7 @@ def get_first_non_repeating(sequence, buffer_size) -> int:
 def main():
     content = pathlib.Path('./input.txt').read_text()
     print('Part 1:', get_first_non_repeating(content, 4))
+    print('Part 2:', get_first_non_repeating(content, 14))
 
 
 if __name__ == '__main__':
