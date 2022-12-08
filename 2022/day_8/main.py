c5b7e248c9e27f70dbe3c8947378abe9b9caa235
sigmanificient <edhyjox@gmail.com>
2022-12-08 22:42:14 +0100

    :sparkles: Add solution for part 2

--- 2022/day_8/main.py
+++ 2022/day_8/main.py
@@ -1,3 +1,5 @@
+import functools
+import operator
 import pathlib
 
 BOARD_SIZE = 99
@@ -6,20 +8,50 @@ BOARD_SIZE = 99
 def is_visible(board, x, y):
     return any(
         board[y][x] > max(trees)
-        for trees in (
-            board[y][:x],
-            board[y][x + 1:],
-            tuple(line[x] for line in board[:y]),
-            tuple(line[x] for line in board[y + 1:])
-        )
+        for trees in get_neighbours_tree(board, x, y)
     )
 
 
 def count_visible(board):
     return sum(
         is_visible(board, x, y)
-        for x in range(1, BOARD_SIZE - 1) for y in range(1, BOARD_SIZE - 1)
+        for x in range(1, BOARD_SIZE - 1)
+        for y in range(1, BOARD_SIZE - 1)
     ) + (BOARD_SIZE - 1) * 4
+
+
+def get_neighbours_tree(board, x, y):
+    return (
+        board[y][:x][::-1],
+        board[y][x + 1:],
+        tuple(line[x] for line in board[:y])[::-1],
+        tuple(line[x] for line in board[y + 1:])
+    )
+
+
+def get_first_non_visible_from(h, trees):
+    for idx, tree in enumerate(trees, start=1):
+        if tree >= h:
+            return idx
+    return len(trees)
+
+
+def scenic_core(board, x, y):
+    return functools.reduce(
+        operator.mul,
+        (
+            get_first_non_visible_from(board[y][x], trees)
+            for trees in get_neighbours_tree(board, x, y)
+        )
+    )
+
+
+def get_highest_scenic_score(board):
+    return max(
+        scenic_core(board, x, y)
+        for x in range(BOARD_SIZE)
+        for y in range(BOARD_SIZE)
+    )
 
 
 def main():
@@ -27,6 +59,7 @@ def main():
     board = [line for line in content.splitlines()]
 
     print('Part 1:', count_visible(board))
+    print('Part 2:', get_highest_scenic_score(board))
 
 
 if __name__ == '__main__':
