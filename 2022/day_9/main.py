636602bd6ba20d759e54a17eae65a15df110a582
sigmanificient <edhyjox@gmail.com>
2022-12-09 18:21:27 +0100

    :construction: Generalized logic (dont work)

--- 2022/day_9/main.py
+++ 2022/day_9/main.py
@@ -1,14 +1,15 @@
 from __future__ import annotations
 
 import pathlib
-from dataclasses import dataclass
 from typing import Literal, cast, Set, Tuple
 
 
-@dataclass
 class Point:
-    x: int
-    y: int
+
+    def __init__(self, x, y):
+        self.previous = None
+        self.x = x
+        self.y = y
 
     def is_touching(self, other: Point) -> bool:
         return (
@@ -20,43 +21,64 @@ class Point:
         )
 
     def move(self, x: int, y: int):
+        self.previous = Point(self.x, self.y)
         self.x += x
         self.y += y
+
+    def copy(self, other: Point):
+        self.previous = Point(self.x, self.y)
+        self.x = other.x
+        self.y = other.y
 
 
 class Rope:
 
-    def __init__(self):
-        self.head = Point(0, 0)
-        self.tail = Point(0, 0)
+    def __init__(self, knots: int):
+        if knots < 2:
+            raise ValueError('Rope must have at least 2 knots')
 
+        self.knots = [Point(0, 0) for _ in range(knots)]
         self.prints: Set[Tuple[int, int]] = set()
+
+    @property
+    def head(self) -> Point:
+        return self.knots[0]
+
+    @property
+    def tail(self) -> Point:
+        return self.knots[-1]
 
     def move_head(self, direction: Literal['L', 'R', 'U', 'D']):
         dx: int = (direction == 'L') - (direction == 'R')
         dy: int = (direction == 'U') - (direction == 'D')
 
         self.head.move(dx, dy)
-        if not self.tail.is_touching(self.head):
-            self.tail = Point(self.head.x - dx, self.head.y - dy)
+        self.propagate_movement()
 
         self.prints.add((self.tail.x, self.tail.y))
+
+    def propagate_movement(self):
+        for head, tail in zip(self.knots, self.knots[1:]):
+            if not tail.is_touching(head):
+                tail.copy(head.previous)
+
+    def solve(self, lines) -> int:
+        for line in lines:
+            move = cast(Literal['L', 'R', 'U', 'D'], line[0])
+            value = int(line[1:])
+
+            for i in range(value):
+                self.move_head(move)
+
+        return len(self.prints)
 
 
 def main():
     content = pathlib.Path('./input.txt').read_text()
     lines = content.splitlines()
 
-    rope = Rope()
-
-    for line in lines:
-        move = cast(Literal['L', 'R', 'U', 'D'], line[0])
-        value = int(line[1:])
-
-        for i in range(value):
-            rope.move_head(move)
-
-    print(len(rope.prints))
+    rope = Rope(2)
+    print("Part 1:", rope.solve(lines))
 
 
 if __name__ == '__main__':
