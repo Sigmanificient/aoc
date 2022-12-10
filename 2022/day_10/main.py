bd864708b0635cff5697b65ae32cdc0b67d8b13c
sigmanificient <edhyjox@gmail.com>
2022-12-10 13:44:09 +0100

    :bento: Add solution for part 2

--- 2022/day_10/main.py
+++ 2022/day_10/main.py
@@ -1,10 +1,7 @@
 import pathlib
 
 
-def main():
-    content = pathlib.Path('./input.txt').read_text()
-    lines = content.splitlines()
-
+def get_cycles_values(lines):
     cycles = [1]
     for line in lines:
         if line == 'noop':
@@ -15,11 +12,35 @@ def main():
         cycles.append(cycles[-1])
         cycles.append(cycles[-1] + n)
 
-    t = 0
-    for cycle_n in (20, 60, 100, 140, 180, 220):
-        t += cycles[cycle_n - 1] * cycle_n
+    cycles.pop()
+    return cycles
 
-    print(t)
+
+def get_target_registers(cycles):
+    return sum(
+        cycles[cycle_n - 1] * cycle_n
+        for cycle_n in (20, 60, 100, 140, 180, 220)
+    )
+
+
+def show_crt(cycles):
+    def chunk(lst, size):
+        return [lst[i:i + size] for i in range(0, len(lst), size)]
+
+    return '\n' + '\n'.join(
+        ''.join(
+            ('##' if abs(idx - x) < 2 else '  ')
+            for idx, x in enumerate(ctr_line)
+        ) for ctr_line in chunk(cycles, 40)
+    )
+
+
+def main():
+    content = pathlib.Path('./input.txt').read_text()
+    cycles = get_cycles_values(content.splitlines())
+
+    print('Part 1:', get_target_registers(cycles))
+    print('Part 2:', show_crt(cycles))
 
 
 if __name__ == '__main__':
