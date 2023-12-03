bbbaccb8fe54af65c3d771d3ae237a9c64317305
Sigmanificient <edhyjox@gmail.com>
2023-12-03 11:42:53 +0100

    Add solution for day 1 - part 2

--- 2023/day_01/main.c
+++ 2023/day_01/main.c
@@ -8,6 +8,26 @@
 #include <unistd.h>
 
 #define TO_DIGIT(c) (int)(c & 0xf)
+#define SSTR_UNPACK(s) s, sizeof(s) - 1
+
+typedef struct {
+    char *name;
+    size_t size;
+} cmps;
+
+const cmps DIGITS_NAMES[] = {
+    SSTR_UNPACK("one"),
+    SSTR_UNPACK("two"),
+    SSTR_UNPACK("three"),
+    SSTR_UNPACK("four"),
+    SSTR_UNPACK("five"),
+    SSTR_UNPACK("six"),
+    SSTR_UNPACK("seven"),
+    SSTR_UNPACK("eight"),
+    SSTR_UNPACK("nine"),
+};
+
+const size_t DIGITS_COUNT = sizeof(DIGITS_NAMES) / sizeof DIGITS_NAMES[0];
 
 int read_from_file(char const *filepath, char *buff, size_t size)
 {
@@ -46,6 +66,18 @@ void part_1(size_t size, char buff[size])
     printf("=> %d\n", res.acc);
 }
 
+void part_2(size_t size, char buff[size])
+{
+    for (size_t i = 0; i < size; i++)
+        if (buff[i] == '\0')
+            buff[i] = '\n';
+    for (int i = 0; buff[i] != '\0'; i++)
+        for (int j = 0; j < DIGITS_COUNT; j++)
+            if (!strncmp(buff + i, DIGITS_NAMES[j].name, DIGITS_NAMES[j].size))
+                buff[i] = (j + 1) + '0';
+    part_1(size, buff);
+}
+
 int main(int argc, char **argv)
 {
     struct stat st;
@@ -62,5 +94,6 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     buff[st.st_size] = '\0';
     part_1(st.st_size, buff);
+    part_2(st.st_size, buff);
     return EXIT_SUCCESS;
 }
