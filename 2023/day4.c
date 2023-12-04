1969d76ceda0180ab0c86a1d33cd75a150ac689b
Sigmanificient <edhyjox@gmail.com>
2023-12-04 18:28:03 +0100

    Add solution for day 4 - part 2

--- 2023/day4.c
+++ 2023/day4.c
@@ -7,6 +7,7 @@
 typedef struct {
     int win;
     int trials;
+    int lines;
 } game_size_t;
 
 static
@@ -23,23 +24,23 @@ int parse_number_pad(char **linep)
 }
 
 static
-int get_score(char *line, int nb_win, int nb_test)
+int get_match_count(game_size_t *gs, char *line)
 {
-    int winnings[nb_win];
-    int player[nb_test];
-    int score = 0;
+    int winnings[gs->win];
+    int player[gs->trials];
+    int count = 0;
 
     line = strchr(line, ':') + 1;
-    for (int i = 0; i < nb_win; i++)
+    for (int i = 0; i < gs->win; i++)
         winnings[i] = parse_number_pad(&line);
     line += 2;
-    for (int i = 0; i < nb_test; i++)
+    for (int i = 0; i < gs->trials; i++)
         player[i] = parse_number_pad(&line);
-    for (int i = 0; i < nb_win; i++)
-        for (int j = 0; j < nb_test; j++)
+    for (int i = 0; i < gs->win; i++)
+        for (int j = 0; j < gs->trials; j++)
             if (player[j] == winnings[i])
-                score++;
-    return (score) ? 1 << (score - 1) : 0;
+                count++;
+    return count;
 }
 
 static
@@ -53,16 +54,46 @@ void parse_game_size(game_size_t *gs, char *anyline)
     gs->trials = (int)(strlen(p) / 3);
 }
 
+static
+void compute_game_score(game_size_t *gs, char *line)
+{
+    int total = 0;
+    int score;
+    int wsize = gs->lines + gs->win;
+    int weights[wsize];
+    int sum = 0;
+    int i = 0;
+
+    for (int j = 0; j < wsize; j++)
+        weights[j] = 1;
+    for (; line != NULL; line = strtok(NULL, "\n")) {
+        score = get_match_count(gs, line);
+        total += (score) ? 1 << (score - 1) : 0;
+        sum += weights[i];
+        for (int j = 0; j < score; j++)
+            weights[i + j + 1] += weights[i];
+        i++;
+    }
+    printf("Part 1: %d\n", total);
+    printf("Part 2: %d\n", sum);
+}
+
+static
+int count_lines(char *buff)
+{
+    int i = 0;
+
+    for (; *buff != '\0'; buff++)
+        if (*buff == '\n')
+            i++;
+    return i;
+}
+
 void day4_solver(size_t size, char buff[size])
 {
+    game_size_t gs = { .lines = count_lines(buff) };
     char *line = strtok(buff, "\n");
-    int total = 0;
-    game_size_t gs;
 
-    if (line == NULL)
-        return;
     parse_game_size(&gs, line);
-    for (; line != NULL; line = strtok(NULL, "\n"))
-        total += get_score(line, gs.win, gs.trials);
-    printf("-> %d\n", total);
+    compute_game_score(&gs, line);
 }
