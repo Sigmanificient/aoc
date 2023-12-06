7f81b280ed5d70a0298b0cb6a0c11534b129967f
Sigmanificient <edhyjox@gmail.com>
2023-12-06 15:38:53 +0100

    Add solution for day 6 - part 2

--- 2023/day6.c
+++ 2023/day6.c
@@ -1,44 +1,75 @@
 #include <limits.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "aoc.h"
 
 typedef struct {
-    char count;
-    int time[CHAR_MAX];
-    int distance[CHAR_MAX];
+    long time;
+    long distance;
 } race_t;
 
 static
-int parse_ints(int *arr, char *line)
+int parse_line(long *arr, size_t dt, char *line)
 {
     int count = 0;
 
     for (; *line != '\0';) {
         for (; *line == ' '; line++);
-        arr[count++] = strpnum(&line);
+        *arr = strpnum(&line);
+        arr = (long *)(((char *)arr) + dt);
+        count++;
     }
     return count;
 }
 
+static
+int compute_win_scenarios(race_t *race)
+{
+    int count = 0;
+
+    for (int t = 0; t < race->time; t++)
+        if (((race->time - t) * t) > race->distance)
+            count++;
+    return count;
+}
+
+static
+void concat_digit(long *out, long i)
+{
+    int base = 1;
+    long tmp = i;
+
+    for (; tmp != 0; tmp /= 10)
+        base *= 10;
+    *out *= base;
+    *out += i;
+}
+
+static
+void concat(race_t *out, race_t *in)
+{
+    concat_digit(&out->time, in->time);
+    concat_digit(&out->distance, in->distance);
+}
+
 void day6_solver(size_t size, char buff[size])
 {
-    race_t race = { 0 };
+    race_t race[CHAR_MAX] = { 0 };
     char *line = strtok(buff, "\n");
     int total = 1;
-    int count;
+    int race_count = parse_line(
+        &race[0].time, sizeof *race,
+        strchr(line, ':') + 1
+    );
 
-    race.count = (char)parse_ints(race.time, strchr(line, ':') + 1);
     line = strtok(NULL, "\n");
-    parse_ints(race.distance, strchr(line, ':') + 1);
-    for (int i = 0; i < race.count; i++) {
-        count = 0;
-        for (int t = 0; t < race.time[i]; t++) {
-            if (((race.time[i] - t) * t) > race.distance[i])
-                count++;
-        }
-        total *= count;
-    }
+    parse_line(&race[0].distance, sizeof *race, strchr(line, ':') + 1);
+    for (int i = 0; i < race_count; i++)
+        total *= compute_win_scenarios(&race[i]);
     printf("Part 1: %d\n", total);
+    for (int i = 1; i < race_count; i++)
+        concat(&race[0], &race[i]);
+    printf("Part 2: %d\n", compute_win_scenarios(&race[0]));
 }
