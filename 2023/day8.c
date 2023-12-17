6e1e2163d9366bc09f71eea2b8dd55d52b3b9c34
Sigmanificient <edhyjox@gmail.com>
2023-12-17 22:43:14 +0100

    Add solution for day 8 - part 2

--- 2023/day8.c
+++ 2023/day8.c
@@ -13,20 +13,19 @@ typedef struct path {
 } path_t;
 
 static
-path_t *find_path(path_t *paths, const char *name, int count_path)
+path_t *find_path(path_t *paths, const char *name, size_t count_path)
 {
-    for (int i = 0; i < count_path; i++)
+    for (size_t i = 0; i < count_path; i++)
         if (!strncmp(paths[i].code, name, 3))
             return &paths[i];
     return NULL;
 }
 
-void day8_solver(size_t size, char buff[size])
+static
+size_t parse_file(path_t *paths)
 {
-    path_t paths[1024];
-    char *instructions = strtok(buff, "\n");
     char *line = strtok(NULL, "\n");
-    int count_path = 0;
+    size_t count_path = 0;
 
     for (; line != NULL; line = strtok(NULL, "\n")) {
         paths[count_path].code = line;
@@ -34,20 +33,85 @@ void day8_solver(size_t size, char buff[size])
         paths[count_path].right = strchr(paths[count_path].left, ' ') + 1;
         count_path++;
     }
+    return count_path;
+}
 
-    size_t len = strlen(instructions);
+static
+size_t get_map_length(path_t *paths, size_t count_path, const char *ins)
+{
+    size_t len = strlen(ins);
     size_t i = 0;
+
+    if (find_path(paths, "ZZZ", count_path) == NULL)
+        return 0;
     for (
         path_t *c = find_path(paths, "AAA", count_path);
         strncmp(c->code, "ZZZ", 3) != 0;
-    ) {
-        printf("Going %.3s\n", (instructions[i] == 'L') ? c->left : c->right);
+    )
         c = find_path(
-            paths,
-            (instructions[i % len] == 'L') ? c->left : c->right,
+            paths, (ins[i++ % len] == 'L') ? c->left : c->right,
             count_path
         );
-        i++;
+    return i;
+}
+
+static
+unsigned int ngcd(const unsigned int *n, size_t count)
+{
+    unsigned int res = *n;
+    unsigned int x;
+    unsigned int y;
+
+    for (size_t i = 1; i < count; i++) {
+        x = n[i];
+        while (x % res) {
+            y = x % res;
+            x = res;
+            res = y;
+        }
     }
-    printf("Part 1: %zu\n", i);
+    return res;
+}
+
+static
+unsigned long compute_map_length(unsigned int *vals, size_t count)
+{
+    unsigned int gcd = ngcd(vals, count);
+    unsigned long res = 1;
+
+    for (size_t i = 0; i < count; i++)
+        res *= vals[i] / gcd;
+    return res * gcd;
+}
+
+static
+size_t get_ghost_map_legnth(path_t *paths, size_t count_path, const char *ins)
+{
+    path_t *ghosts[1024];
+    unsigned int count[1024];
+    size_t ghosts_count = 0;
+    size_t len = strlen(ins);
+    char *path;
+
+    for (size_t i = 0; i < count_path; i++)
+        if (paths[i].code[2] == 'A')
+            ghosts[ghosts_count++] = &paths[i];
+    for (size_t i = 0; i < ghosts_count; i++) {
+        for (; ghosts[i]->code[2] != 'Z'; count[i]++) {
+            path = ((ins[count[i] % len] == 'L')
+                ? ghosts[i]->left : ghosts[i]->right);
+            ghosts[i] = find_path(paths, path, count_path);
+        }
+    }
+    return compute_map_length(count, ghosts_count);
+}
+
+void day8_solver(size_t size, char buff[size])
+{
+    path_t paths[1024];
+    char *ins = strtok(buff, "\n");
+    size_t count_path = parse_file(paths);
+
+    printf("Part 1: %zu\n", get_map_length(paths, count_path, ins));
+    printf("Part 2: %zu\n", get_ghost_map_legnth(paths, count_path, ins));
 }
