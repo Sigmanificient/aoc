f8c749a652a9c80a89ce1721d9114ddaefbe3ebb
Sigmanificient <edhyjox@gmail.com>
2023-12-18 23:03:20 +0100

    Add solution for day 9 - part 2

--- 2023/day9.c
+++ 2023/day9.c
@@ -30,18 +30,37 @@ void reduce_line(int *vals, int count)
         reduce_line(vals, count - 1);
 }
 
+static
+void revlst(int *vals, int count)
+{
+    int half = count >> 1;
+    int swp;
+
+    for (int i = 0; i < half; i++) {
+        swp = vals[i];
+        vals[i] = vals[count - i - 1];
+        vals[count - i - 1] = swp;
+    }
+}
+
 void day9_solver(size_t size, char buff[size])
 {
-    char *line = strtok(buff, "\n");
+    char *line = strtok(strdup(buff), "\n");
     int vals[1024];
+    int cpy[1024];
     int count;
-    int total = 0;
+    int total[2] = { 0 };
 
     for (; line != NULL; line = strtok(NULL, "\n")) {
         for (count = 0; *line != '\0'; strnum(&line, &vals[count++]))
             for (; *line == ' '; line++);
+        memcpy(cpy, vals, (size_t)count * sizeof(int));
         reduce_line(vals, count);
-        total += extrapolate_value(vals, count);
+        total[0] += extrapolate_value(vals, count);
+        revlst(cpy, count);
+        reduce_line(cpy, count);
+        total[1] += extrapolate_value(cpy, count);
     }
-    printf("Part 1: %d\n", total);
+    printf("Part 1: %d\n", total[0]);
+    printf("Part 2: %d\n", total[1]);
 }
