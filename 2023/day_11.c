1ab495711c235814fd9c9fb82516186bc6877a71
Sigmanificient <edhyjox@gmail.com>
2023-12-31 08:18:55 +0100

    Add solution for day 11 - part 2

--- 2023/day_11.c
+++ 2023/day_11.c
@@ -84,36 +84,6 @@ bool is_col_empty(size_t x, size_t height, char **arr)
     return true;
 }
 
-static
-void space_expand_empty_lines(array_t *sp)
-{
-    char *swp;
-    const char newline[] = "\n";
-    for (size_t y = 0; y < sp->height; y++) {
-        if (!is_line_empty(sp->width, sp->vals[y]))
-            continue;
-        swp = sp->vals[sp->height];
-        memmove(&sp->vals[y + 1], &sp->vals[y], (sp->height - y) * sizeof(char *));
-        sp->vals[y] = swp;
-        sp->height++;
-        y++;
-    }
-
-    for (size_t x = 0; x < sp->width; x++) {
-        if (!is_col_empty(x, sp->height, sp->vals))
-            continue;
-        for (size_t y = 0; y < sp->height; y++) {
-            memmove(&sp->vals[y][x + 1], &sp->vals[y][x], sp->width - x);
-            sp->vals[y][x] = '.';
-        }
-        sp->width++;
-        x++;
-    }
-
-    for (size_t y = 0; y < sp->height; y++)
-        memcpy(&sp->vals[y][sp->width], newline, sizeof(newline));
-}
-
 static __attribute_pure__
 size_t space_count_galaxies(array_t *sp)
 {
@@ -147,35 +117,91 @@ pos_t *register_galaxy_positions(array_t *sp, size_t count)
     return gpos;
 }
 
+enum {
+    AXIS_X,
+    AXIS_Y,
+    AXIS_COUNT
+};
+
 static
-size_t compute_galaxy_distance(pos_t *ga, pos_t *gb)
+size_t compute_galaxy_distance(
+    pos_t *ga, pos_t *gb,
+    pos_t *exp_lut, size_t exp_factor)
 {
+    pos_t r_ga = {
+        .x = exp_lut[ga->x].x * exp_factor + ga->x,
+        .y = exp_lut[ga->y].y * exp_factor + ga->y
+    };
+    pos_t r_gb = {
+        .x = exp_lut[gb->x].x * exp_factor + gb->x,
+        .y = exp_lut[gb->y].y * exp_factor + gb->y
+    };
+
     return (
-        ((ga->x > gb->x) ? (ga->x - gb->x) : (gb->x - ga->x))
-        + ((ga->y > gb->y) ? (ga->y - gb->y) : (gb->y - ga->y))
+        ((r_ga.x > r_gb.x) ? (r_ga.x - r_gb.x) : (r_gb.x - r_ga.x))
+        + ((r_ga.y > r_gb.y) ? (r_ga.y - r_gb.y) : (r_gb.y - r_ga.y))
     );
+}
+
+static
+pos_t *expand_lut_create(array_t *sp)
+{
+    size_t count = 0;
+    size_t height = sp->width > sp->height ? sp->width : sp->height;
+    pos_t *lut = malloc(height * sizeof *lut);
+
+    if (lut == NULL)
+        return NULL;
+
+    for (size_t i = 0; i < sp->width; i++) {
+        count += is_col_empty(i, sp->height, sp->vals);
+        lut[i].x = count;
+    }
+    count = 0;
+    for (size_t i = 0; i < sp->height; i++) {
+        count += is_line_empty(sp->width, sp->vals[i]);
+        lut[i].y = count;
+    }
+    return lut;
+}
+
+static
+size_t sum_distance(
+    pos_t *galaxies, size_t galaxy_count,
+    pos_t *exp_lut, size_t exp_fac)
+{
+    size_t total = 0;
+
+    for (size_t i = 0; i < galaxy_count; i++)
+        for (size_t j = i; j < galaxy_count; j++)
+            total += compute_galaxy_distance(
+                &galaxies[i], &galaxies[j], exp_lut, exp_fac);
+    return total;
 }
 
 void day11_solver(size_t size, char buff[size])
 {
     array_t *space = space_create(size, buff);
     pos_t *galaxies = NULL;
-    size_t galaxy_count;
-    size_t total = 0;
+    pos_t *exp_lut = NULL;
+    size_t gal_count;
 
     if (space == NULL)
         goto end;
-    galaxy_count = space_count_galaxies(space);
-    space_expand_empty_lines(space);
-    galaxies = register_galaxy_positions(space, galaxy_count);
+
+    exp_lut = expand_lut_create(space);
+    if (exp_lut == NULL)
+        goto end;
+
+    gal_count = space_count_galaxies(space);
+    galaxies = register_galaxy_positions(space, gal_count);
     if (galaxies == NULL)
         goto end;
 
-    for (size_t i = 0; i < galaxy_count; i++)
-        for (size_t j = i; j < galaxy_count; j++)
-            total += compute_galaxy_distance(&galaxies[i], &galaxies[j]);
-    printf("Part 1: %zu\n", total);
+    printf("Part 1: %zu\n", sum_distance(galaxies, gal_count, exp_lut, 1));
+    printf("Part 2: %zu\n", sum_distance(galaxies, gal_count, exp_lut, 999999));
 end:
     free(galaxies);
+    free(exp_lut);
     free(space);
 }
