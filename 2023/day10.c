88b79e3b5b7d1865b646c556cf8bf8a04b924759
Sigmanificient <edhyjox@gmail.com>
2023-12-31 01:01:14 +0100

    Rewrite the entire solution to use a better approach

--- 2023/day10.c
+++ 2023/day10.c
@@ -1,137 +1,147 @@
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "aoc.h"
-#define INVALID ((pos_t){ 0 })
 
-typedef enum {
-    NORTH = 0,
-    EAST = 1,
-    SOUTH = 2,
-    WEST = 3
-} cardinal_t;
+#define TILESIZE 3
+
+static
+const char MAZE_GROWTH[CHAR_MAX][TILESIZE][TILESIZE] = {
+    ['J'] = { ".X.", "XX.", "..." },
+    ['L'] = { ".X.", ".XX", "..." },
+    ['F'] = { "...", ".XX", ".X." },
+    ['7'] = { "...", "XX.", ".X." },
+    ['-'] = { "...", "XXX", "..." },
+    ['|'] = { ".X.", ".X.", ".X." },
+    ['.'] = { "...", "...", "..." },
+    ['S'] = { "...", ".S.", "..." }
+};
 
 typedef struct {
-    int x;
-    int y;
-    cardinal_t card;
+    size_t x;
+    size_t y;
 } pos_t;
 
 typedef struct {
-    size_t width;
     size_t height;
-} dim_t;
+    size_t width;
+    char **vals;
+    pos_t animal;
+} array_t;
 
 static
-const char CURRENT_TILE_LUT[16] = {
-    [0x5] = 'J',
-    [0x6] = 'L',
-    [0x9] = '7',
-    [0xa] = 'F',
-};
-
-
-static
-const cardinal_t BASE_CARD_LUT[CHAR_MAX] = {
-    ['J'] = EAST,
-    ['L'] = SOUTH,
-    ['7'] = EAST,
-    ['F'] = NORTH
-};
-
-static
-const pos_t MOVEMENT_LUT[CHAR_MAX][4] = {
-    ['J'] = {
-        [EAST] = { 0, -1, NORTH },
-        [SOUTH] = { -1, 0, WEST },
-    },
-    ['L'] = {
-        [WEST] = { 0, -1, NORTH },
-        [SOUTH] = { 1, 0, EAST },
-    },
-    ['7'] = {
-        [NORTH] = { -1, 0, WEST },
-        [EAST] = { 0, 1, SOUTH },
-    },
-    ['F'] = {
-        [NORTH] = { 1, 0, EAST },
-        [WEST] = { 0, 1, SOUTH },
-    },
-    ['-'] = {
-        [EAST] = { 1, 0, EAST },
-        [WEST] = { -1, 0, WEST },
-    },
-    ['|'] = {
-        [NORTH] = { 0, -1, NORTH },
-        [SOUTH] = { 0, 1, SOUTH },
-    },
-};
-
-static
-int get_pos(
-    dim_t *dim, char buff[dim->height][dim->width + 1],
-    int x, int y)
+void maze_set_animal_tile(array_t *maze)
 {
-    if (x < 0 || y < 0)
-        return '.';
-    if (x > (int)dim->width || y > (int)dim->height)
-        return '.';
-    return buff[y][x];
+    pos_t a = maze->animal;
+
+    if (a.x <= TILESIZE || a.y >= TILESIZE)
+        __builtin_unreachable();
+    if (a.x == maze->width - TILESIZE || a.y >= maze->height - TILESIZE)
+        __builtin_unreachable();
+
+    maze->vals[a.y][a.x] = 'X';
+    maze->vals[a.y - 1][a.x] = maze->vals[a.y - 2][a.x];
+    maze->vals[a.y + 1][a.x] = maze->vals[a.y + 2][a.x];
+    maze->vals[a.y][a.x - 1] = maze->vals[a.y][a.x - 2];
+    maze->vals[a.y][a.x + 1] = maze->vals[a.y][a.x + 2];
 }
 
 static
-void find_animal(
-    dim_t *dim, char buff[dim->height][dim->width + 1],
-    pos_t *p)
+void maze_paint_from_string(array_t *maze, const char *buff)
 {
-    int dir = 0;
-    ptrdiff_t offset = strchr((char *)buff, 'S') - (char *)buff;
+    const char (*p)[TILESIZE];
+    size_t x = 0;
+    size_t y = 0;
 
-    p->y = (int)offset / (int)(dim->width + 1);
-    p->x = (int)offset % (int)(dim->width + 1);
-    dir += (strchr("LF-", get_pos(dim, buff, p->x - 1, p->y)) != NULL) << 0;
-    dir += (strchr("7J-", get_pos(dim, buff, p->x + 1, p->y)) != NULL) << 1;
-    dir += (strchr("7F|", get_pos(dim, buff, p->x, p->y - 1)) != NULL) << 2;
-    dir += (strchr("LJ|", get_pos(dim, buff, p->x, p->y + 1)) != NULL) << 3;
-    buff[p->y][p->x] = CURRENT_TILE_LUT[dir];
+    for (size_t i = 0; buff[i] != '\0'; i++) {
+        if (buff[i] == '\n') {
+            y += TILESIZE;
+            x = 0;
+            continue;
+        }
+
+        if (buff[i] == 'S') {
+            maze->animal.x = x + 1;
+            maze->animal.y = y + 1;
+        }
+
+        p = MAZE_GROWTH[(int)buff[i]];
+        for (size_t off = 0; off < TILESIZE; off++)
+            memcpy(&maze->vals[y + off][x], p[off], TILESIZE);
+        x += TILESIZE;
+    }
+
+    maze_set_animal_tile(maze);
 }
 
 static
-int solve_pipe_maze(dim_t *dim, char buff[dim->height][dim->width + 1])
+array_t *maze_create(size_t size, char buff[size])
 {
-    pos_t p = { 0 };
-    pos_t dir = { .x = 1, .y = 1 };
-    pos_t mov;
-    pos_t target;
-    int count = 0;
+    size_t height = 0;
+    size_t width = 0;
+    array_t *maze;
+    char *p;
 
-    find_animal(dim, buff, &p);
-    p.card = BASE_CARD_LUT[(int)buff[p.y][p.x]];
-    target.x = p.x;
-    target.y = p.y;
-    do {
-        mov = MOVEMENT_LUT[(int)buff[p.y][p.x]][p.card];
-        buff[p.y][p.x] = '!';
-        p.x += mov.x * dir.x;
-        p.y += mov.y * dir.y;
-        p.card = mov.card;
-        count++;
-    } while (p.x != target.x || p.y != target.y);
-    return count;
+    width = TILESIZE * strcspn(buff, "\n");
+    for (size_t i = 0; i < size; i++)
+        height += buff[i] == '\n';
+
+    height *= 3;
+    if (!width || !height)
+        return NULL;
+
+    maze = malloc(sizeof *maze
+        + sizeof(char **) * height
+        + sizeof(char) * (height + 1) * width);
+    if (maze == NULL)
+        return NULL;
+
+    maze->vals = (char **)&maze[1];
+    maze->width = width;
+    maze->height = height;
+
+    p = (char *)&maze->vals[height];
+    for (size_t i = 0; i < height; i++)
+        maze->vals[i] = &p[i * width];
+
+    width /= TILESIZE;
+    maze_paint_from_string(maze, buff);
+    return maze;
+}
+
+static
+size_t maze_flood(
+    array_t *restrict maze,
+    size_t y, size_t x,
+    const char cell[static 2])
+{
+    size_t hit = 0;
+
+    if (maze->vals[y][x] != cell[0])
+        return 0;
+    maze->vals[y][x] = cell[1];
+    if (y > 0)
+        hit += maze_flood(maze, y - 1, x, cell);
+    if ((y + 1) < maze->height)
+        hit += maze_flood(maze, y + 1, x, cell);
+    if (x > 0)
+        hit += maze_flood(maze, y, x - 1, cell);
+    if ((x + 1) < maze->width)
+        hit += maze_flood(maze, y, x + 1, cell);
+    return hit + 1;
 }
 
 void day10_solver(size_t size, char buff[size])
 {
-    int count;
-    dim_t dim = {
-        .width = strcspn(buff, "\n"),
-        .height = 0
-    };
+    array_t *maze = maze_create(size, buff);
+    size_t length;
 
-    for (size_t i = 0; buff[i] != '\0'; i++)
-        if (buff[i] == '\n')
-            dim.height++;
-    count =solve_pipe_maze(&dim, *(char (*)[dim.height][dim.width + 1])(buff));
-    printf("Part 1: %d\n", count >> 1);
+    if (maze == NULL)
+        return;
+
+    length = maze_flood(maze, maze->animal.y, maze->animal.x, "X!");
+    printf("Part 1: %zu\n", length / (TILESIZE << 1));
+    free(maze);
 }
