c23bf700885ab9eb71623718e06efa4f3c3ca936
Sigmanificient <edhyjox@gmail.com>
2024-01-01 22:10:45 +0100

    Add tiny flake with devShell

---
+++ 2023/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [ gcc gnumake ];
+        };
+      });
+}
